8950a31175d6e7ffc71f7a40ac5d2949aed24dcd
kumar <yerramsetti915@gmail.com>
2023-12-08 20:14:42 -0500

    Update main.js with  sending email function

--- js/main.js
+++ js/main.js
@@ -321,3 +321,33 @@
  })
 
 
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var form = this;
+    var data = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            form.reset();
+            document.getElementById('successMessage').style.display = 'block';
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert("Oops! There was a problem submitting your form");
+                }
+            });
+        }
+    }).catch(error => {
+        alert("Oops! There was a problem submitting your form");
+    });
+});
+
